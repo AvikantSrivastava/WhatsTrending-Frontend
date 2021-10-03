8a89e29ef6da9c0d9d3b4dc66124cda9a36495cc
Avikant Srivastava <asdavikant@gmail.com>
2021-10-04 00:58:42 +0530

    other location working

--- src/TrendsTable.svelte
+++ src/TrendsTable.svelte
@@ -1,3 +1,15 @@
+<script>
+  let dataPromise = getData();
+
+  async function getData() {
+    return fetch("https://twitter-trends-daily.herokuapp.com/alltrends").then(
+      (response) => {
+        return response.json();
+      }
+    );
+  }
+</script>
+
 <div class="sm-12 md-4 col sidebar">
   <div class="paper">
     <h3>
@@ -5,6 +17,33 @@
     </h3>
     <div>
       <table id="TrendsTable" />
+      {#await dataPromise}
+        loading
+      {:then data}
+        {#each Object.entries(data.trends) as [location, trends]}
+          {#if location != "key"}
+            <div class="collapsible">
+              <input
+                id="collapsible{location}"
+                type="checkbox"
+                name="collapsible"
+              />
+              <label for="collapsible{location}">{location}</label>
+              <div class="collapsible-body">
+                <ol
+                  style="display: inline-block text-align:left; list-style-position: inside;"
+                >
+                  {#each trends as trend}
+                    <li>
+                      <a href="https://twitter.com/search?q={trend}">{trend}</a>
+                    </li>
+                  {/each}
+                </ol>
+              </div>
+            </div>
+          {/if}
+        {/each}
+      {/await}
     </div>
   </div>
 </div>
